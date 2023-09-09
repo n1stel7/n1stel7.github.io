29edd5fb49fb56c36794ebcb209b939577a807cc
n1stel7 <88253387+n1stel7@users.noreply.github.com>
2023-09-09 11:44:33 -0400

    Add files via upload

--- osu.ppy.sh/main.js
+++ osu.ppy.sh/main.js
@@ -4,9 +4,9 @@
 		scrollHandler()
 	};
 	function scrollHandler() {
-		if (document.documentElement.scrollTop > 30) {
+		if (window.scrollY > 30) {
 			body.classList.add("js-pinned-header");
-		} else if (document.documentElement.scrollTop < 30) {
+		} else if (window.scrollY < 30) {
 			body.classList.remove("js-pinned-header");
 		}
 	}
@@ -16,11 +16,9 @@
 	const sublinks_bg = document.querySelector(".topnav_sublinks_bg");
 	js_links.forEach(link => {
 		link.addEventListener("mouseover", () => {
-			// toadd.forEach(el => { el.classList.remove("hidden") })
 			sublinks_bg.classList.remove("hidden")
 		})
 		link.addEventListener("mouseleave", () => {
-			// toadd.forEach(el => { el.classList.add("hidden") })
 			sublinks_bg.classList.add("hidden")
 		})
 	})
@@ -33,16 +31,22 @@
 
 // mobile menu
 	const menu_links = document.querySelector(".menu_links");
+	const mobile_submenu = document.querySelector(".mobile_submenu");
+
 	menu_links.querySelectorAll(".menu_drop").forEach(link => {
 		link.addEventListener("click", e => {
 			e.preventDefault();
-			if (link.querySelector(".mobile_submenu").classList.contains("none")) {
-				link.querySelector(".item_arrow").classList.remove("tfed")
-				link.querySelector(".mobile_submenu").classList.remove("none");
+			let item_arrow = link.querySelector(".item_arrow");
+			let mobile_submenu = link.querySelector(".mobile_submenu");
+
+			if (mobile_submenu.classList.contains("none")) {
+				item_arrow.classList.remove("tfed")
+				mobile_submenu.classList.remove("none");
 			} else {
-				link.querySelector(".item_arrow").classList.add("tfed")
-				link.querySelector(".mobile_submenu").classList.add("none");
+				item_arrow.classList.add("tfed")
+				mobile_submenu.classList.add("none");
 			}
+
 		})
 	})
 
@@ -50,10 +54,15 @@
 
 
 	mobile_menu = document.querySelector(".mobile_menu");
-	document.querySelector(".toggle").addEventListener("click", e => {
-		if (mobile_menu.classList.contains("none")) {
-			mobile_menu.classList.remove("none")
+	blackout = document.querySelector(".blackout");
+	document.querySelector(".toggle").addEventListener("click", function(e) {
+		if (mobile_menu.classList.contains("unclipped")) {
+			blackout.classList.add("hidden")
+			mobile_menu.classList.remove("unclipped")
+			this.classList.add("tfed")
 		} else {
-			mobile_menu.classList.add("none")
+			mobile_menu.classList.add("unclipped")
+			blackout.classList.remove("hidden")
+			this.classList.remove("tfed")
 		}
 	})
